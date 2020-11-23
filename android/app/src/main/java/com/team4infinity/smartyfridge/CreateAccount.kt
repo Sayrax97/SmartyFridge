b681aa3a30ecf03d4422fe25c21ddfefadfd2240
Sayrax <dusann.jankovic@elfak.rs>
2020-11-23 03:00:12 +0100

    Saving user to FB database

--- android/app/src/main/java/com/team4infinity/smartyfridge/CreateAccount.kt
+++ android/app/src/main/java/com/team4infinity/smartyfridge/CreateAccount.kt
@@ -4,22 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
+import com.team4infinity.smartyfridge.models.User
 
 class CreateAccount : AppCompatActivity() {
 
     private lateinit var auth:FirebaseAuth
+    private lateinit var db:DatabaseReference
     private lateinit var name:EditText
     private lateinit var surname:EditText
     private lateinit var email: EditText
     private lateinit var password:EditText
     private lateinit var confirmPassword:EditText
     private lateinit var createAccountBtn:Button
+    private val TAG = "CreateAccount"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
@@ -39,6 +45,11 @@ class CreateAccount : AppCompatActivity() {
                 Toast.makeText(this, "Your passwords do not match", Toast.LENGTH_SHORT).show()
             else{
                 auth.createUserWithEmailAndPassword(email.text.toString(),password.text.toString()).addOnSuccessListener{authResult->
+                    var uid = authResult.user?.uid
+                    if (uid != null){
+                        val user = User(name.text.toString(), surname.text.toString(), email.text.toString(), password.text.toString())
+                        db.child("users").child(uid).setValue(user)
+                    }
                     val intent=Intent(this,MainActivity::class.java)
                     startActivity(intent)
                 }.addOnFailureListener{authResult->Toast.makeText(this, "Authentication error", Toast.LENGTH_SHORT).show()}
@@ -48,6 +59,7 @@ class CreateAccount : AppCompatActivity() {
 
     private fun init(){
         auth= Firebase.auth
+        db = FirebaseDatabase.getInstance().reference
         name=findViewById(R.id.editName);
         surname=findViewById(R.id.editSurname);
         email=findViewById(R.id.editEmail);
